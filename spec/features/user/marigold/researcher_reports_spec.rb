f524483a5af22ddd32d61d0abfb49435c93519c3
Alan <a-ashbeck@northwestern.edu>
2016-07-14 14:57:38 -0500

    Update new Marigold CSV spec

--- spec/features/user/marigold/researcher_reports_spec.rb
+++ spec/features/user/marigold/researcher_reports_spec.rb
@@ -4,14 +4,13 @@
 require 'uuid'
 require 'fileutils'
 
-def confirm_file(link)
+def select_and_check_file(link, file_name)
   @driver.find_element(link: link).click
-  file = (link == 'Thought' || link == 'Activity') ? "patient#{link}" : link
-  file_path = "#{@download_dir}/#{file.downcase.delete(' ')}.csv"
+  file_path = "#{@download_dir}/#{file_name}.csv"
   File.size(file_path).should be > 0
 end
 
-feature 'Researcher downloads CSV Exports', :marigold do
+feature 'Researcher, downloads CSV exports', :marigold do
   background(:all) do
     @download_dir = File.join(Dir.pwd, UUID.new.generate)
     FileUtils.mkdir_p @download_dir
@@ -26,7 +25,7 @@ feature 'Researcher downloads CSV Exports', :marigold do
     @driver.get "#{ENV['Base_URL']}/users/sign_in"
     @driver.find_element(id: 'user_email').send_keys(ENV['Researcher_Email'])
     @driver.find_element(id: 'user_password')
-           .send_keys(ENV['Researcher_Password'])
+      .send_keys(ENV['Researcher_Password'])
     @driver.find_element(css: '.btn.btn-default').submit
     @driver.get "#{ENV['Base_URL']}/think_feel_do_dashboard/reports"
   end
@@ -36,39 +35,157 @@ feature 'Researcher downloads CSV Exports', :marigold do
     FileUtils.rm_rf @download_dir
   end
 
-  scenario 'Comment' do
-    confirm_file('Comment')
-  end
-
-  scenario 'Goal' do
-    confirm_file('Goal')
-  end
-
-  scenario 'Like' do
-    confirm_file('Like')
-  end
-
-  scenario 'Nudge' do
-    confirm_file('Nudge')
-  end
-
-  scenario 'Off Topic Post' do
-    confirm_file('Off Topic Post')
-  end
-
-  scenario 'Tool Share' do
-    confirm_file('Tool Share')
-  end
-
   scenario 'Emotional Rating' do
-    confirm_file('Emotional Rating')
+    sleep(2)
+    select_and_check_file('Emotional Rating', 'emotionalrating')
   end
 
-  scenario 'Thought' do
-    confirm_file('Thought')
+  scenario 'Gamification: Behavior' do
+    sleep(2)
+    select_and_check_file('Gamification: Behavior', 'behavior')
   end
 
-  scenario 'Activity' do
-    confirm_file('Activity')
+  scenario 'Gamification: Incentive' do
+    sleep(2)
+    select_and_check_file('Gamification: Incentive', 'incentive')
+  end
+
+  scenario 'Gamification: Participant Behavior' do
+    sleep(2)
+    select_and_check_file('Gamification: Participant Behavior',
+                          'participantbehavior')
+  end
+
+  scenario 'Gamification: Participant Incentive' do
+    sleep(2)
+    select_and_check_file('Gamification: Participant Incentive',
+                          'participantincentive')
+  end
+
+  scenario 'Login' do
+    sleep(2)
+    select_and_check_file('Login', 'login')
+  end
+
+  scenario 'Module Page View' do
+    sleep(2)
+    select_and_check_file('Module Page View', 'modulepageview')
+  end
+
+  scenario 'Module Session' do
+    sleep(2)
+    select_and_check_file('Module Session', 'modulesession')
+  end
+
+  scenario 'Participant Profile' do
+    sleep(2)
+    select_and_check_file('Participant Profile', 'participantprofile')
+  end
+
+  scenario 'Practice: Activation Activity' do
+    sleep(2)
+    select_and_check_file('Practice: Activation Activity',
+                          'activationactivity')
+  end
+
+  scenario 'Practice: Gratitude Journal' do
+    sleep(2)
+    select_and_check_file('Practice: Gratitude Journal', 'gratituderecording')
+  end
+
+  scenario 'Practice: Kindness' do
+    sleep(2)
+    select_and_check_file('Practice: Kindness', 'kindness')
+  end
+
+  scenario 'Practice: Mindfulness Activity' do
+    sleep(2)
+    select_and_check_file('Practice: Mindfulness Activity', 'mindfulactivity')
+  end
+
+  scenario 'Practice: Mindfulness Meditation' do
+    sleep(2)
+    select_and_check_file('Practice: Mindfulness Meditation', 'meditation')
+  end
+
+  scenario 'Practice: Positive Experience' do
+    sleep(2)
+    select_and_check_file('Practice: Positive Experience', 'experience')
+  end
+
+  scenario 'Practice: Reappraisal' do
+    sleep(2)
+    select_and_check_file('Practice: Reappraisal', 'reappraisal')
+  end
+
+  scenario 'Practice: Strength' do
+    sleep(2)
+    select_and_check_file('Practice: Strength', 'strength')
+  end
+
+  scenario 'Site Session' do
+    sleep(2)
+    select_and_check_file('Site Session', 'sitesession')
+  end
+
+  scenario 'Skill: Feedback' do
+    sleep(2)
+    select_and_check_file('Skill: Feedback', 'lessonfeedback')
+  end
+
+  scenario 'Skill: Slide View' do
+    sleep(2)
+    select_and_check_file('Skill: Slide View', 'lessonslideview')
+  end
+
+  scenario 'Skill: Viewing' do
+    sleep(2)
+    select_and_check_file('Skill: Viewing', 'lessonviewing')
+  end
+
+  scenario 'Social: Comment' do
+    sleep(2)
+    select_and_check_file('Social: Comment', 'comment')
+  end
+
+  scenario 'Social: Like' do
+    sleep(2)
+    select_and_check_file('Social: Like', 'like')
+  end
+
+  scenario 'Social: Nudge' do
+    sleep(2)
+    select_and_check_file('Social: Nudge', 'nudge')
+  end
+
+  scenario 'Social: On My Mind Statements' do
+    sleep(2)
+    select_and_check_file('Social: On My Mind Statements', 'offtopicpost')
+  end
+
+  scenario 'Task Completion' do
+    sleep(2)
+    select_and_check_file('Task Completion', 'taskcompletion')
+  end
+
+  scenario 'Tool Access' do
+    sleep(2)
+    select_and_check_file('Tool Access', 'toolaccess')
+  end
+
+  scenario 'Unsubscribed Phone Numbers' do
+    sleep(2)
+    select_and_check_file('Unsubscribed Phone Numbers',
+                          'smsdeliveryerrorrecord')
+  end
+
+  scenario 'User Agent' do
+    sleep(2)
+    select_and_check_file('User Agent', 'useragent')
+  end
+
+  scenario 'Video Session' do
+    sleep(2)
+    select_and_check_file('Video Session', 'videosession')
   end
 end
