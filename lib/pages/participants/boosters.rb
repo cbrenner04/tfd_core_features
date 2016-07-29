3f153d811bb684888df496aba281c3d53f8475c7
Alan <a-ashbeck@northwestern.edu>
2016-07-29 10:33:05 -0500

    Add boosters spec and PO

---
+++ lib/pages/participants/boosters.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module Participants
+  # page object for the Boosters tool in Marigold
+  class Boosters
+    include Capybara::DSL
+
+    def has_thank_you_visible?
+      has_text 'Thank you for visiting the MARIGOLD booster session!'
+    end
+
+    def visible?
+      has_text 'Trying to practice lots of skills at once can be ' \
+               'overwhelming. To start with, choose just one skill, ' \
+               'and we\'ll suggest a few easy options for practicing it.'
+    end
+  end
+end
--- spec/features/participant/marigold/boosters_spec.rb
+++ spec/features/participant/marigold/boosters_spec.rb
@@ -1,2 +1,18 @@
 # frozen_string_literal: true
 # filename: ./spec/features/participant/marigold/boosters_spec.rb
+
+require './lib/pages/participants/boosters.rb'
+require './lib/pages/participants/navigation.rb'
+
+feature 'Boosters', :marigold, sauce: sauce_labs do
+  scanerio 'link takes participant to boosters' do
+    visit "#{ENV['Base_URL']}/booster_session"
+
+    expect(boosters).to have_thank_you_visisble
+
+    navigation.alt_next
+
+    expect(boosters).to be_visible
+  end
+
+end
